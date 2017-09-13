b274131abef35045dc594eeed30cdcd7336fa75f
MarikoOta <mariko296@gmail.com>
2017-09-13 18:34:16 +0900

    first commit

---
+++ sico/sico/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sico
+//
+//  Created by 太田麻莉子 on 2017/09/10.
+//  Copyright © 2017年 太田麻莉子. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
