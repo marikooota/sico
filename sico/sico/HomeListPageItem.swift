b68ebafd902da6d7221fa1109d2a9b602715f7de
MarikoOta <mariko296@gmail.com>
2017-09-19 00:19:26 +0900

    タイムライン作成

--- sico/sico/HomeListPageItem.swift
+++ sico/sico/HomeListPageItem.swift
@@ -8,9 +8,12 @@
 
 import UIKit
 
-class HomeListPageItem: UIViewController {
+class HomeListPageItem: UIViewController,UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var loginButton: UIBarButtonItem!
+    @IBOutlet weak var timelineTableView: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         loginButton.setTitleTextAttributes([ NSFontAttributeName: UIFont(name: "Noto Sans Mono CJK JP", size: 17)!], for: UIControlState.normal)
@@ -22,15 +25,25 @@ class HomeListPageItem: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    //各セルの要素を設定する
+    internal func tableView(_ timelineTableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.subtitle, reuseIdentifier: "timeLineCell")
+        let cell = timelineTableView.dequeueReusableCell(withIdentifier: "timeLineCell")! as UITableViewCell
+//        cell.textLabel!.text = program[indexPath.row]
+        let imageView = cell.viewWithTag(1) as! UIImageView
+        imageView.image = UIImage(named: "timeline_image1")
+        let label = cell.viewWithTag(2) as! UILabel
+        label.text = "unfull"
+        return cell
     }
-    */
-
+    
+    //セルに表示する文字列の個数を返す
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    //セルが選択された時に動作する
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        return
+    }
 }
